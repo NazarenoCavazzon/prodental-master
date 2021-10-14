a4e0cbfb58d367849615dd6ca5c25bba8e5d438d
Nazareno Cavazzon <cavazzonnazareno@gmail.com>
2021-10-13 23:10:46 -0300

    changes

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": "Giorgiolomas19!",
+    "password": null,
     "database": "prodental",
     "host": "127.0.0.1",
     "dialect": "mysql",
