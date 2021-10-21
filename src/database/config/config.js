12a524076e6a46fab47149b73a784453f3a5de2a
MANUEL AHUMADA <mahumada@lasalleflorida.edu.ar>
2021-10-21 14:35:32 -0300

    Changed timezone in sequelize

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -5,7 +5,10 @@ module.exports = {
     "database": "prodental",
     "host": "127.0.0.1",
     "dialect": "mysql",
-    "timezone": 'Z',
+    dialectOptions: {
+      useUTC: false, // for reading from database
+    },
+    timezone: '-03:00', // for writing to database
     define: {
       underscored: true,
       timestamps: false
