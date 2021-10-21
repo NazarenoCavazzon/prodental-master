2e95e560a74ae4b1f1453c61c618e3195bbca913
MANUEL AHUMADA <mahumada@lasalleflorida.edu.ar>
2021-10-21 19:46:44 -0300

    fixed bug

--- dotfiles/insert-staff.sql
+++ dotfiles/insert-staff.sql
@@ -1,1 +1,1 @@
-INSERT into staffs(name,matricula) values ("Rodolfo Griant", "M39854"), ("Ramiro Soldan", "M345584"), ("Gimena Robles", "M54887"), ("Maximiliano Grant", "M11111"), ("Milagros Perez Rodriguez", "M22222");
+INSERT into staffs(name,matricula) values ("Rodolfo Griant", "M39854"), ("Ramiro Soldan", "M345584"), ("Gimena Robles", "M54887"), ("Maximiliano Grant", "M11111"), ("Milagros Perez", "M22222");
