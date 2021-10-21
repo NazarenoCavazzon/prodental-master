199f68d320abefef1e9afe2d8c70b2f3d35daddf
MANUEL AHUMADA <mahumada@lasalleflorida.edu.ar>
2021-10-21 14:19:05 -0300

    Changed sql script

--- dotfiles/onlySQLScript.sh
+++ dotfiles/onlySQLScript.sh
@@ -3,17 +3,21 @@
 clear
 echo "Starting sql config..."
 
+clear
 echo "Creating database... 1/3"
 sudo mysql < createDB.sql
 
+clear
 echo "Creating tables in db... 2/3"
 sudo mysql prodental < tables.sql
 
+clear
 echo "Poblating database... 3/3"
 sudo mysql prodental < insert-testimonials.sql
 sudo mysql prodental < insert-treatments.sql
 sudo mysql prodental < insert-staff.sql
 
+clear
 echo "Finished script."
-sleep(1)
+sleep 1
 clear
