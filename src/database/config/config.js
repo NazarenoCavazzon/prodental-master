cbcfc97932192c1d8dc373b69107604a675b7a90
MANUEL AHUMADA <mahumada@lasalleflorida.edu.ar>
2021-10-21 15:49:07 -0300

    CHanged timezone config

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -5,9 +5,6 @@ module.exports = {
     "database": "prodental",
     "host": "127.0.0.1",
     "dialect": "mysql",
-    dialectOptions: {
-      useUTC: false, // for reading from database
-    },
     timezone: '-03:00', // for writing to database
     define: {
       underscored: true,
