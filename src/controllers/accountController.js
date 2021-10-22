88a2816189c7f4c340b13264b14ce906f3abf98e
Nazareno Cavazzon <cavazzonnazareno@gmail.com>
2021-10-22 11:42:23 -0300

    Revert "Revert "Changes""
    This reverts commit 861e9e10222e49cf202ae0a28c4c75c152e05074.

--- src/controllers/accountController.js
+++ src/controllers/accountController.js
@@ -3,222 +3,219 @@ const loadLang = require('./loadLangController');
 const moment = require('moment');
 moment.locale('es');
 
+
+
 module.exports = {
     index: async (req,res) =>{
-
         let lang = null;
         if (req.cookies.lang == undefined){
-          lang='eng';
-          res.cookie("lang","eng");
+        lang='esp';
+        res.cookie("lang","eng");
         }else{
-          lang=req.cookies.lang;
+        lang=req.cookies.lang;
         }
         let language = loadLang(lang);
-  
 
-        if (req.session.userLog){
-            let user = await db.User.findOne({
-                where:{
-                    id: req.session.userLog
-                }
-            })
-
-            let turns = await db.Turn.findAll({
-                where:{
-                    user_id: req.session.userLog
-                },
-                include: [
-                    'treatment'
-                ]
-            }) 
-
-            turns.forEach(turn => {
-                turn.setDataValue('date',moment(turn.date).format('dddd DD MMMM YYYY'))
-            });
-
-
-            return res.render('account',{
-                user: user, 
-                turns: turns ,
-                title: 'Account | Prodental',
-                accountDat: language._account
-            })
-        }else{
-            return res.redirect('/login')
+        let user = await db.User.findOne({
+            where:{
+                id: req.session.userLog
+            }
+        })
+        if (user.dataValues.is_admin){
+            res.redirect('/admin/users');
         }
+
+        let turns = await db.Turn.findAll({
+            where:{
+                user_id: req.session.userLog
+            },
+            include: [
+                'treatment'
+            ]
+        }) 
+
+        turns.forEach(turn => {
+            turn.setDataValue('date',moment(turn.date).format('dddd DD MMMM YYYY'))
+        });
+
+
+        return res.render('account',{
+            user: user, 
+            turns: turns ,
+            title: 'Account | Prodental',
+            accountDat: language._account
+        })
     },
     turnTreatment: async (req,res) =>{
 
-        let lang = null;
-        if (req.cookies.lang == undefined){
-          lang='eng';
-          res.cookie("lang","eng");
-        }else{
-          lang=req.cookies.lang;
-        }
-        let language = loadLang(lang);
+        if (req.cookies.logged){
+            let lang = null;
+            if (req.cookies.lang == undefined){
+            lang='eng';
+            res.cookie("lang","eng");
+            }else{
+            lang=req.cookies.lang;
+            }
+            let language = loadLang(lang);
 
-        if (req.session.userLog){
-            let user = await db.User.findOne({
-                where:{
-                    id: req.session.userLog
-                }
-            })
-            let treatments = await db.Treatment.findAll();
-            return res.render('account/turn-treatment',
-            { 
-                treatments: treatments, 
-                title: "Turn | Prodental", 
-                user: user,
-                accountDat: language._account
-            });
+            if (req.session.userLog){
+                let user = await db.User.findOne({
+                    where:{
+                        id: req.session.userLog
+                    }
+                })
+                let treatments = await db.Treatment.findAll();
+                return res.render('account/turn-treatment',
+                { 
+                    treatments: treatments, 
+                    title: "Turn | Prodental", 
+                    user: user,
+                    accountDat: language._account
+                });
+            }else{
+                return res.redirect('/login')
+            }
         }else{
             return res.redirect('/login')
         }
-
-
     },
     processTurnTreatment: async (req,res) =>{
 
-        let lang = null;
-        if (req.cookies.lang == undefined){
-            lang='eng';
-        res.cookie("lang","eng");
-        }else{
-            lang=req.cookies.lang;
-        }
-        let language = loadLang(lang);  
-
-        if (req.session.userLog){
-
-            let user = await db.User.findOne({
-                where:{
-                    id: req.session.userLog
-                }
-            })
-
-        
-            let turnsByDaysArray = []; 
-            let days = [];
-
-    
-            // TRAIGO DESDE LA DB
-            let turns = await db.Turn.findAll({
-                where: {
-                    user_id: null,
-                    treatment_id: req.body.treatment
-                }
-            });
-    
-               
-    
-            // OBTENEMOS SOLO LOS DIAS 
-            for (let i = 0; i < turns.length; i++){
-
-                let date = moment(turns[i].date).format('DD MMMM YYYY');
-                
-                if (!days.includes(date)){
-                    days.push(date);
-                    turnsByDaysArray.push([]);
-                }
+        if (req.cookies.logged){
+            let lang = null;
+            if (req.cookies.lang == undefined){
+                lang='eng';
+            res.cookie("lang","eng");
+            }else{
+                lang=req.cookies.lang;
             }
-    
-            // FILTRAMOS Y ORDENAMOS SEGUN DIAS
-            for (let k = 0; k < days.length; k++){
+            let language = loadLang(lang);  
 
-                for (let l = 0; l < turns.length; l++){
+            if (req.session.userLog){
 
-                    let date = moment(turns[l].date).format('DD MMMM YYYY');
-                    let time =  moment(turns[l].date).format('h:mm a');
-
-                    if (date.includes(days[k])){
-                        turns[l].setDataValue('date', date);  
-                        turns[l].setDataValue('time', time);  
-                        turnsByDaysArray[k].push(turns[l].dataValues)
+                let user = await db.User.findOne({
+                    where:{
+                        id: req.session.userLog
                     }
+                })
+            
+                let turnsByDaysArray = []; 
+                let days = [];
+        
+                // TRAIGO DESDE LA DB
+                let turns = await db.Turn.findAll({
+                    where: {
+                        user_id: null,
+                        treatment_id: req.body.treatment
+                    }
+                });
+                // OBTENEMOS SOLO LOS DIAS 
+                for (let i = 0; i < turns.length; i++){
 
+                    let date = moment(turns[i].date).format('DD MMMM YYYY');
+                    
+                    if (!days.includes(date)){
+                        days.push(date);
+                        turnsByDaysArray.push([]);
+                    }
                 }
-            }
-                
+                // FILTRAMOS Y ORDENAMOS SEGUN DIAS
+                for (let k = 0; k < days.length; k++){
 
-            
-            
-            return res.render('account/turn-date', { 
-                turns: turnsByDaysArray, 
-                treatment_id: req.body.treatment, 
-                title: "Turn | Prodental", 
-                user:user,
-                accountDat: language._account
-            });
+                    for (let l = 0; l < turns.length; l++){
+
+                        let date = moment(turns[l].date).format('DD MMMM YYYY');
+                        let time =  moment(turns[l].date).format('h:mm a');
+
+                        if (date.includes(days[k])){
+                            turns[l].setDataValue('date', date);  
+                            turns[l].setDataValue('time', time);  
+                            turnsByDaysArray[k].push(turns[l].dataValues)
+                        }
+
+                    }
+                }
+                return res.render('account/turn-date', { 
+                    turns: turnsByDaysArray, 
+                    treatment_id: req.body.treatment, 
+                    title: "Turn | Prodental", 
+                    user:user,
+                    accountDat: language._account
+                });
+            }else{
+                return res.redirect('/login')
+            }
         }else{
             return res.redirect('/login')
         }
-
-
-
-        
-        
     },
     turnDate: async (req,res) =>{
 
-        
-        let lang = null;
-        if (req.cookies.lang == undefined){
-          lang='eng';
-          res.cookie("lang","eng");
-        }else{
-          lang=req.cookies.lang;
-        }
-        let language = loadLang(lang);
+        if (req.cookies.logged){
+            let lang = null;
+            if (req.cookies.lang == undefined){
+            lang='eng';
+            res.cookie("lang","eng");
+            }else{
+            lang=req.cookies.lang;
+            }
+            let language = loadLang(lang);
 
-        if (req.session.userLog){
-            let user = await db.User.findOne({
-                where:{
-                    id: req.session.userLog
-                }
-            })
-            let treatments = await db.Treatment.findAll();
+            if (req.session.userLog){
+                let user = await db.User.findOne({
+                    where:{
+                        id: req.session.userLog
+                    }
+                })
+                let treatments = await db.Treatment.findAll();
 
-            
-            return res.render('account/turn-date',
-            { 
-                user: user, 
-                treatments: treatments ,
-                title: "Turn | Prodental" ,
-                accountDat: language._account
-            });
+                
+                return res.render('account/turn-date',
+                { 
+                    user: user, 
+                    treatments: treatments ,
+                    title: "Turn | Prodental" ,
+                    accountDat: language._account
+                });
+            }else{
+                return res.redirect('/login')
+            }
         }else{
             return res.redirect('/login')
-        }
-        
+        }        
     },
     processTurnDate: async (req,res) =>{
 
+        if (req.cookies.logged){
+            await db.Turn.update({
+                treatment_id: req.body.treatmentId,
+                user_id: req.session.userLog,
+            },{
+                where: {
+                    id: req.body.dateTurn
+                }
+            })
 
-        await db.Turn.update({
-            treatment_id: req.body.treatmentId,
-            user_id: req.session.userLog,
-        },{
-            where: {
-                id: req.body.dateTurn
-            }
-        })
-
-        return res.redirect('/account/');
-        
-        
+            return res.redirect('/account/');
+        }else{
+            return res.redirect('/login')
+        }
     },
     turnDelete: async (req,res) =>{
-        
-        await db.Turn.update({
-            user_id: null,
-        },{
-            where: {
-                id: req.body.turnId
-            }
-        })
 
-        return res.redirect('/account')
+        if (req.cookies.logged){        
+            await db.Turn.update({
+                user_id: null,
+            },{
+                where: {
+                    id: req.body.turnId
+                }
+            })
+
+            return res.redirect('/account')
+        }else{
+            return res.redirect('/login')
+        }
     },
     
 }
--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -6,135 +6,175 @@ const tratmentsJSON = require('../../public/json/eng/treatments.json')
 module.exports = {
 
     index: async(req,res) =>{
-        const user = await db.User.findOne({where: {id: req.session.userLog}});
-        console.log(user.dataValues);
-        if(user.dataValues.is_admin){
-            res.redirect('/admin/users');
-        }else{
-            res.redirect('/account');
+        if (req.cookies.logged){
+            const user = await db.User.findOne({where: {id: req.session.userLog}});
+            if(user.dataValues.is_admin){
+                res.redirect('/admin/users');
+            }else{
+                res.redirect('/account');
+            }
+        } else {
+            res.redirect('/login');
         }
     },
     users: async (req,res) =>{
+        if (req.cookies.logged){
+            const user = await db.User.findOne({where: {id: req.session.userLog}});
 
-        // if (req.session.userLog != 0){
-        //     res.redirect('/account');
-        // }else{
+            if (!user.dataValues.is_admin){
+                res.redirect('/account');
+            }else{
 
-            let users = await db.User.findAll({include:['turns']});
+                let users = await db.User.findAll({include:['turns']});
 
-            console.log(users);
-
-            return res.render('admin/users', {
-                title: 'Admin users | Dentalpro',
-                users: users
-            });
-        // }
+                return res.render('admin/users', {
+                    title: 'Admin users | Dentalpro',
+                    users: users
+                });
+            }
+        } else {
+            res.redirect('/login');
+        }
     },
     staff: async (req,res) =>{
+        if (req.cookies.logged){
+            const user = await db.User.findOne({where: {id: req.session.userLog}});
 
-        // if (req.session.userLog != 0){
-        //     res.redirect('/account');
-        // }else{
-            let staffs = await db.Staff.findAll();
-            
-            return res.render('admin/staff', {
-                title: 'Admin staff | Dentalpro',
-                staffs: staffs
-            });
-        // }
+            if (!user.dataValues.is_admin){
+                res.redirect('/account');
+            }else{
+                let staffs = await db.Staff.findAll();
+                
+                return res.render('admin/staff', {
+                    title: 'Admin staff | Dentalpro',
+                    staffs: staffs
+                });
+            }
+        } else {
+            res.redirect('/login');
+        }
     },
     turns: async (req,res) =>{
 
-        // if (req.session.userLog != 0){
-        //     res.redirect('/account');
-        // }else{
-
-            const turns = await db.Turn.findAll({include:['treatment', 'user']});
-            const treatments = await db.Treatment.findAll();
-            const users = await db.User.findAll();
-
-            let lang = null;
-            if (req.cookies.lang == undefined){
-                lang='eng';
-                
-                res.cookie("lang","eng");
+        if (req.cookies.logged){
+            const user = await db.User.findOne({where: {id: req.session.userLog}});
+            if (!user.dataValues.is_admin){
+                res.redirect('/account');
             }else{
-                lang=req.cookies.lang;
+
+                const turns = await db.Turn.findAll({include:['treatment', 'user']});
+                const treatments = await db.Treatment.findAll();
+                const users = await db.User.findAll();
+
+                let lang = null;
+                if (req.cookies.lang == undefined){
+                    lang='eng';
+                    
+                    res.cookie("lang","eng");
+                }else{
+                    lang=req.cookies.lang;
+                }
+                
+                moment.locale(lang);
+                
+                turns.forEach(turn => {
+                    turn.setDataValue('dateParsed',moment(turn.date).format('dddd DD MMMM YYYY'))
+                });
+
+
+                return res.render('admin/turns', {
+                    title: 'Admin turns | Dentalpro',
+                    turns: turns,
+                    treatments: treatments,
+                    users: users
+                });
             }
-            
-            moment.locale(lang);
-            
-            turns.forEach(turn => {
-                turn.setDataValue('dateParsed',moment(turn.date).format('dddd DD MMMM YYYY'))
-            });
-
-
-            return res.render('admin/turns', {
-                title: 'Admin turns | Dentalpro',
-                turns: turns,
-                treatments: treatments,
-                users: users
-            });
-        // }
+        } else {
+            res.redirect('/login');
+        }
     },
-    account: (req,res) =>{
+    account: async (req,res) =>{
 
-        // if (req.session.userLog != 0){
-        //     res.redirect('/account');
-        // }else{
-            return res.render('admin/account', {
-                title: 'Admin account | Dentalpro'
-            });
-        // }
+        if (req.cookies.logged){
+            const user = await db.User.findOne({where: {id: req.session.userLog}});
+            if (!user.dataValues.is_admin){
+                res.redirect('/account');
+            }else{
+                return res.render('admin/account', {
+                    title: 'Admin account | Dentalpro'
+                });
+            }
+        } else {
+            res.redirect('/login');
+        }
     },
     treatments: async (req,res) =>{
 
-        // if (req.session.userLog != 0){
-        //     res.redirect('/account');
-        // }else{
+        if (req.cookies.logged){
+            const user = await db.User.findOne({where: {id: req.session.userLog}});
+            if (!user.dataValues.is_admin){
+                res.redirect('/account');
+            }else{
+                const treatments = await db.Treatment.findAll();
 
-            const treatments = await db.Treatment.findAll();
-
-            return res.render('admin/treatments', {
-                title: 'Admin treatments | Dentalpro',
-                treatments: treatments
-            });
-        // }
+                return res.render('admin/treatments', {
+                    title: 'Admin treatments | Dentalpro',
+                    treatments: treatments
+                });
+            }
+        } else {
+            res.redirect('/login');
+        }
     },
     turnCreate: async (req,res) =>{
-        if (req.body.user!='null'){
-            await db.Turn.create({
-                treatment_id: req.body.treatment,
-                user_id: req.body.user,
-                date: req.body.datetime
-            })
-        }else{
-            await db.Turn.create({
-                treatment_id: req.body.treatment,
-                date: req.body.datetime
-            })
+
+        if (req.cookies.logged){
+            if (req.body.user!='null'){
+                await db.Turn.create({
+                    treatment_id: req.body.treatment,
+                    user_id: req.body.user,
+                    date: req.body.datetime
+                })
+            }else{
+                await db.Turn.create({
+                    treatment_id: req.body.treatment,
+                    date: req.body.datetime
+                })
+            }
+            return res.redirect('/admin/turns');
+        } else {
+            res.redirect('/login');
         }
-        return res.redirect('/admin/turns');
     },
     turnDelete: async (req,res) =>{
-        await db.Turn.destroy({
-            where: {
-                id: req.body.deleteTurnId
-            }
-        })
-        return res.redirect('/admin/turns');
+
+        if (req.cookies.logged){
+            await db.Turn.destroy({
+                where: {
+                    id: req.body.deleteTurnId
+                }
+            })
+            return res.redirect('/admin/turns');
+        } else {
+            res.redirect('/login');
+        }
     },
 
     turnEdit: async (req,res) =>{
-        await db.Turn.update({
-            treatment_id: req.body.treatment,
-            user_id:  req.body.user,
-            date:  req.body.datetime,
-        },{
-            where: {
-                id: req.body.idTurn
-            }
-        })
-        return res.redirect('/admin/turns')
+
+        if (req.cookies.logged){
+            await db.Turn.update({
+                treatment_id: req.body.treatment,
+                user_id:  req.body.user,
+                date:  req.body.datetime,
+            },{
+                where: {
+                    id: req.body.idTurn
+                }
+            })
+            return res.redirect('/admin/turns')
+        } else {
+            res.redirect('/login');
+        }
     }
 }
